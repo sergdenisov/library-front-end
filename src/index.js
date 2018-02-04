97adcd6e8cb3f08f81cb75d5513522cf3702b436
Sergey Denisov <xsergeantx@mail.ru>
2018-02-04 18:04:28 +0300

    Add redux, react-redux and redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "spectre.css/dist/spectre.css";
-import App from "./components/App/App";
+import Index from "./components/Index/Index";
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(React.createElement(Index), document.getElementById("root"));
